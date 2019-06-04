2d0b0ff2a9d325f9dd476f4353ad38b8dea55f15
yangobeil <ninigobeil@hotmail.com>
2019-06-04 13:44:30 -0400

    Use links to save pictures in given folder

--- scraper.py
+++ scraper.py
@@ -16,8 +16,22 @@ url = 'https://www.google.com/search?q=' + word + '&source=lnms&tbm=isch'
 page = requests.get(url, headers=headers)
 soup = BeautifulSoup(page.content, features="lxml")
 
-count = 0
+directory = 'C:\\Users\\Yan\\Dropbox\\Machine learning\\images google'
+
+links = []
+types = []
 for image in soup.find_all('div', {'class':'rg_meta'}):
     link = json.loads(image.text)['ou']
-    count += 1
-print(count)
+    type = json.loads(image.text)['ity']
+    if type:
+        links.append(link)
+        types.append(type)
+
+num_images = len(links)
+
+for i in range(num_images):
+    img = requests.get(links[i])
+    file_name = str(i) + '.' + types[i]
+    path = os.path.join(directory, file_name)
+    with open(path, 'wb') as image_file:
+        image_file.write(img.content)
